34d8373b5e8a566069807f17f0016eaeea3c38ae
melfry <melfry@WORKGOOSE>
2016-04-21 09:12:11 -0700

    First Sync

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+        
+        public GradeStatistics ComputeStatistics()
+        {
+            return new GradeStatistics();
+        }
+
+
+        List<float> grades;
+
+    }
+}
---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("How much sleep did you have last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+            if (hoursOfSleep <8)
+            {
+                Console.WriteLine("You must feel tired");
+            }
+            else
+            {
+                Console.WriteLine("You must be rested.");
+            }
+        }
+    }
+}
